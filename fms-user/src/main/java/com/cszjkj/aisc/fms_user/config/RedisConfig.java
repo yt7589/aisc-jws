291639d6d88cae86f52cc8d64258df7f3c159c44
yantao <yt7589@qq.com>
2021-04-20 21:20:07 +0800

    add redis cache web address

--- fms-user/src/main/java/com/cszjkj/aisc/fms_user/config/RedisConfig.java
+++ fms-user/src/main/java/com/cszjkj/aisc/fms_user/config/RedisConfig.java
@@ -7,11 +7,19 @@ import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.util.Objects;
+
+// https://www.jianshu.com/p/da7fe207f9e3
+// https://blog.csdn.net/weixin_44216706/article/details/106607257
+// https://blog.csdn.net/shaoming314/article/details/109153487
+// https://www.cnblogs.com/caizhaokai/p/11037610.html
 
 @Configuration
 @EnableCaching
@@ -26,13 +34,16 @@ public class RedisConfig extends CachingConfigurerSupport {
     // 缓存管理器
     @Bean
     public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(Objects.requireNonNull(redisTemplate.getConnectionFactory()));
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(
+                Objects.requireNonNull(redisTemplate.getConnectionFactory()));
 
-        Jackson2JsonRedisSerializer<RedisUser> serializer = new Jackson2JsonRedisSerializer<>(RedisUser.class);
+        /*Jackson2JsonRedisSerializer<RedisUser> serializer = new Jackson2JsonRedisSerializer<>(RedisUser.class);
 
         RedisSerializationContext<RedisUser, RedisUser> serializationContext = RedisSerializationContext.fromSerializer(serializer);
         RedisCacheConfiguration redisCacheConfiguration =  RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(serializationContext.getValueSerializationPair());
 
         return new RedisCacheManager(redisCacheWriter,redisCacheConfiguration);
+         */
+        return null;
     }
 }
