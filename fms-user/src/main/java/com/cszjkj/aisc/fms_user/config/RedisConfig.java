66ddc6f6fb52aabac9a5e1f1da91ce4e6ab35de3
yantao <yt7589@qq.com>
2021-04-21 13:32:49 +0800

    add UserDTO definition

--- fms-user/src/main/java/com/cszjkj/aisc/fms_user/config/RedisConfig.java
+++ fms-user/src/main/java/com/cszjkj/aisc/fms_user/config/RedisConfig.java
@@ -2,44 +2,17 @@ package com.cszjkj.aisc.fms_user.config;
 
 
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.util.Objects;
-
-// https://www.jianshu.com/p/da7fe207f9e3
-// https://blog.csdn.net/weixin_44216706/article/details/106607257
-// https://blog.csdn.net/shaoming314/article/details/109153487
-// https://www.cnblogs.com/caizhaokai/p/11037610.html
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
-    @Value("${spring.redis.host}")
-    private String host;
-    @Value("${spring.redis.port}")
-    private int port;
-    @Value("${spring.redis.timeout}")
-    private int timeout;
-
     @Bean("redisTemplate")
     @Primary
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
