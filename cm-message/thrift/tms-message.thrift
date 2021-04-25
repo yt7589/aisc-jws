1497e64c1998d7407700effdc4115ba5171fee84
yantao <yt7589@qq.com>
2021-04-25 16:12:55 +0800

    add cm-message thrift API definition

---
+++ cm-message/thrift/tms-message.thrift
@@ -0,0 +1,7 @@
+namespace py api
+namespace java com.cszjkj.aisc.tms_message
+
+service MessageServiceApi {
+    bool send_sms(1:string mobile_phone, 2:string msg);
+    bool send_email(1:string email_addr, 2:string msg);
+}
